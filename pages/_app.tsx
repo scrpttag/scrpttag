d743c765338551b87f3753be360f5414d69b9a94
Gabor Molnar <gabormolnar92@gmail.com>
2020-11-23 01:17:08 +0100

    SCRPT-1: Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const MyApp = ({Component, pageProps}) => {
+    return (
+        <>
+            <Head>
+                <title>Scrpttag</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+                      integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+                      crossOrigin="anonymous"/>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import { NextPage } from 'next';
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Scrpttag</title>
+            </Head>
+        </>
+    )
+}
+
+export default Home
