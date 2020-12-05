7c9063e4b4af1122adde0a8b3ff17d8af286b20e
Gabor Molnar <gabormolnar92@gmail.com>
2020-12-05 22:18:10 +0100

    SCRPT-1: Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,13 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       <Head>
         <title>Scrpttag</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#181a1b" />
+        <meta name="msapplication-TileColor" content="#f5f6f7" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Component {...pageProps} />
     </>
