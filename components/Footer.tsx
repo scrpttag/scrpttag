83f3ed38b284c5973b8fa80d0e165c7546bc44d7
Gabor Molnar <gabormolnar92@gmail.com>
2021-03-26 22:23:56 +0100

    SCRPT-1: Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,37 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
 
-type FooterProps = {}
+type FooterLinkProps = {
+  href: string
+  isLast?: boolean
+}
 
 const SocialLinkList = styled.ul`
   writing-mode: tb;
 `
 
-const Footer: FunctionComponent<FooterProps> = () => {
+const FooterLink: FunctionComponent<FooterLinkProps> = ({ href, children, isLast = false }) => {
+  const listClasses = 'uppercase font-bold text-gray-300 hover:text-gray-100 cursor-pointer transition-colors'
+  const margin = isLast ? 'mb-auto' : 'mb-1'
+  return (
+    <li className={`${margin} ${listClasses}`}>
+      <a className="p-4 inline-flex items-center" href={href} target="_blank">
+        {children}
+      </a>
+    </li>
+  )
+}
+
+const Footer: FunctionComponent = () => {
   return (
     <footer className="flex fixed text-sm right-0 h-full w-32 dark:bg-gray-900 border-r-2 dark:border-gray-600 items-center flex-col p-6">
       <SocialLinkList className="flex list-none p-0 text-gray-100 h-full">
-        <li className="mb-1 uppercase font-bold text-gray-300 hover:text-gray-100 cursor-pointer transition-colors">
-          <a className="p-4 inline-flex items-center" href="https://www.facebook.com/scrpttag" target="_blank">
-            Facebook
-          </a>
-        </li>
-        <li className="mb-auto uppercase font-bold text-gray-300 hover:text-gray-100 cursor-pointer transition-colors">
-          <a className="p-4 inline-flex items-center" href="https://www.instagram.com/scrpttag" target="_blank">
-            Instagram
-          </a>
-        </li>
-        <li className="p-4 uppercase font-bold text-gray-300">2020 scrpttag.com</li>
+        <FooterLink href="https://www.facebook.com/scrpttag">Facebook</FooterLink>
+        <FooterLink href="https://twitter.com/scrpttag">Twitter</FooterLink>
+        <FooterLink href="https://www.instagram.com/scrpttag" isLast>
+          Instagram
+        </FooterLink>
+        <li className="p-4 uppercase font-bold text-gray-300">{new Date().getFullYear()} scrpttag.com</li>
       </SocialLinkList>
     </footer>
   )
