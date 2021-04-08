f3347c82942620c7e6c32ad3e50b49001b26aed2
Gabor Molnar <gabormolnar92@gmail.com>
2021-04-08 23:34:48 +0200

    SCRPT-6: Fix footer and main page on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
   return (
     <LayoutBase>
       <section className="mt-10 lg:mt-20">
-        <h1 className="font-mono text-gray-100 w-96 text-6xl lg:text-8xl">Hello, Scrpttag!</h1>
+        <h1 className="font-mono text-gray-100 w-full text-4xl sm:w-96 sm:text-6xl lg:text-8xl">Hello, Scrpttag!</h1>
         <h2 className="text-gray-300 mt-10 text-2xl lg:mt-20 lg:text-3xl">A blog about my path in tech era</h2>
       </section>
       <section className="flex flex-col mt-10 pt-10 lg:flex-row lg:items-stretch lg:mt-20 lg:pt-20 border-t-2 dark:border-gray-600">
